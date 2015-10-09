608abdf282dcaf870933b02c3aece6e5565583d8
Jerome Eteve <jerome@broadbean.com>
2015-10-09 11:04:04 +0100

    Build jira client object

--- lib/App/JIRAPrint.pm
+++ lib/App/JIRAPrint.pm
@@ -4,16 +4,82 @@ package App::JIRAPrint;
 use Moose;
 use Log::Any qw/$log/;
 
+=head1 NAME
+
+App::JIRAPrint - Print JIRA Tickets on post it sheets
+
+=cut
+
 use autodie qw/:all/;
 use Cwd;
 use File::Spec;
 use Hash::Merge;
 
+use JIRA::REST;
+
 
 # Config stuff.
 has 'config' => ( is => 'ro', isa => 'HashRef', lazy_build => 1);
-
 has 'config_files' => ( is => 'ro' , isa => 'ArrayRef[Str]' , lazy_build => 1);
+
+# Operation properties
+has 'url' => ( is => 'ro', isa => 'Str', lazy_build => 1 );
+has 'username' => ( is => 'ro', isa => 'Str' , lazy_build => 1);
+has 'password' => ( is => 'ro', isa => 'Str' , lazy_build => 1);
+
+has 'project' => ( is => 'ro', isa => 'Str' , lazy_build => 1 );
+has 'sprint'  => ( is => 'ro', isa => 'Str' , lazy_build => 1 );
+
+has 'jql' => ( is => 'ro', isa => 'Str', lazy_build => 1);
+
+# Objects
+has 'jira' => ( is => 'ro', isa => 'JIRA::REST', lazy_build => 1);
+
+sub _build_jira{
+    my ($self) = @_;
+    $log->info("Accessing JIRA At ".$self->url()." as '".$self->username()."' (+password)");
+    return JIRA::REST->new( $self->url() , $self->username() , $self->password() );
+}
+
+
+sub _build_url{
+    my ($self) = @_;
+    return $self->config()->{url} // die "Missing url ".$self->config_place()."\n";
+}
+
+sub _build_username{
+    my ($self) = @_;
+    return $self->config()->{username} // die "Missing username ".$self->config_place()."\n";
+}
+
+sub _build_password{
+    my ($self) = @_;
+    return $self->config()->{password} // die "Missing password ".$self->config_place()."\n";
+}
+
+sub _build_project{
+    my ($self) = @_;
+    return $self->config()->{project} // die "Missing project ".$self->config_place()."\n";
+}
+
+sub _build_sprint{
+    my ($self) = @_;
+    return $self->config()->{sprint} // die "Missing sprint ".$self->config_place()."\n";
+}
+
+sub _build_jql{
+    my ($self) = @_;
+    return $self->config()->{jql} //
+        'project = "'.$self->project().'" and Sprint = "'.$self->sprint().'" ORDER BY status, assignee, created'
+}
+
+sub config_place{
+    my ($self) = @_;
+    if( $self->has_config_files() && @{ $self->config_files() } ){
+        return 'in config files: '.join(', ', @{$self->config_files()} );
+    }
+    return 'in memory config';
+}
 
 sub _build_config{
     my ($self) = @_;
@@ -40,6 +106,7 @@ sub _build_config_files{
       );
     my @files = ();
     foreach my $candidate ( @candidates ){
+        $log->debug("Looking for $candidate");
         if( -r $candidate ){
             $log->info("Found config file '$candidate'");
             push @files , $candidate;
--- t/config.t
+++ t/config.t
@@ -4,8 +4,17 @@ use Test::More;
 
 use App::JIRAPrint;
 
-my $j = App::JIRAPrint->new({ config_files => [ 't/config1.conf' ,  't/config2.conf' ] });
-is_deeply( $j->config() , { foo => [ 'bar2' , 'bar1' ] } );
+{
+    my $j = App::JIRAPrint->new({ config_files => [ 't/config1.conf' ,  't/config2.conf' ] });
+    is_deeply( $j->config() , { foo => [ 'bar2' , 'bar1' ] } );
+    is( $j->config_place() , 'in config files: '.join(', ' , @{[ 't/config1.conf',  't/config2.conf'] } ) );
+}
+
+{
+    my $j = App::JIRAPrint->new({ config => {} });
+    is( $j->config_place() , 'in memory config' );
+}
+
 
 done_testing();
 
